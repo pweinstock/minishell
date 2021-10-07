b11199f052fd1058cf92bdaeb6f0f6df19b49179
Kevin Hirsig <khirsig@2-D-1.42heilbronn.de>
2021-10-07 10:29:59 +0200

    version 0.31

--- srcs/builtin/bltin_cd.c
+++ srcs/builtin/bltin_cd.c
@@ -6,7 +6,7 @@
 /*   By: khirsig <khirsig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 08:39:20 by khirsig           #+#    #+#             */
-/*   Updated: 2021/10/07 09:25:46 by khirsig          ###   ########.fr       */
+/*   Updated: 2021/10/07 10:25:48 by khirsig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 
 void	bltin_cd(t_data *data, char **cmd)
 {
-	// char *cwd;
+	char *cwd;
 
-	(void)data;
 	// printf("Test: |%s|\n", cmd[1]);
 	if (chdir(cmd[1]) == -1)
 	{
@@ -25,9 +24,9 @@ void	bltin_cd(t_data *data, char **cmd)
 		ft_putstr_fd("\n", 2);
 		return ;
 	}
-	// cwd = getcwd(NULL, 0);
-	// cwd += ft_revchrsrch(cwd, '/') + 1;
-	// free(data->path_prefix);
-	// data->path_prefix = ft_strdup(cwd);
+	cwd = getcwd(NULL, 0);
+	cwd += ft_revchrsrch(cwd, '/') + 1;
+	free(data->path_prefix);
+	data->path_prefix = ft_strdup(cwd);
 	return ;
 }
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: khirsig <khirsig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 10:50:41 by khirsig           #+#    #+#             */
-/*   Updated: 2021/10/07 10:18:28 by khirsig          ###   ########.fr       */
+/*   Updated: 2021/10/07 10:28:49 by khirsig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,14 @@ int	main(int argc, char **argv, char **envp)
 	data = malloc(sizeof(t_data));
 	ft_bzero(data, sizeof(t_data));
 	data->envp = envp;
-	data->fd_in = open("fd_in", O_CREAT|O_RDWR|O_TRUNC, S_IRWXU);
+	// data->fd_in = open("fd_in", O_CREAT|O_RDWR|O_TRUNC, S_IRWXU);
 	//close(data.fd_in);
-	data->fd_out = open("fd_out", O_CREAT|O_RDWR|O_TRUNC, S_IRWXU);
-	close(data->fd_out);
+	// data->fd_out = open("fd_out", O_CREAT|O_RDWR|O_TRUNC, S_IRWXU);
+	// close(data->fd_out);
+	// dup2(data->fd_in, 0);
+	// dup2(data->fd_out, 1);
+	data->fd_in = 0;
+	data->fd_out = 1;
 	data->path_prefix = ft_strdup("minishell");
 	(void)argc;
 	(void)argv;
@@ -32,15 +36,17 @@ int	main(int argc, char **argv, char **envp)
 		// write(1, data->path_prefix, ft_strlen(data->path_prefix));
 		// write(1, "\033[1;33m ✗\033[0m ", 17);
 		char	*str = readline(data->path_prefix);
-		if (str == NULL)
-			break;
-		if (str && *str)
-			add_history(str);
+		// if (str == NULL)
+		// 	break;
+		// if (str && *str)
+		// 	add_history(str);
 		pipex(str, envp, data);
+		// dup2(0, data->fd_in);
+		// dup2(1, data->fd_out);
 		// specifier(&data, str);
 		//printf("%s\n", str);
-		signal(SIGQUIT, SIG_IGN);
-		signal(SIGINT, signal_handler);
+		// signal(SIGQUIT, SIG_IGN);
+		// signal(SIGINT, signal_handler);
 	}
 	// int fd = open("test", O_CREAT);
 	// printf("fd: %d\n", fd);
